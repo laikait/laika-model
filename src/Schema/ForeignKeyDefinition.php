5c9335f6b03a8e3458d219c6b54b16b14b52e10f
Showket Ahmed <strblackhawk@gmail.com>
2026-03-05 00:14:48 +0600

    ForeignKeyDefinition Coments Added

--- src/Schema/ForeignKeyDefinition.php
+++ src/Schema/ForeignKeyDefinition.php
@@ -11,41 +11,68 @@ class ForeignKeyDefinition
 {
     private array $definition;
 
+    /**
+     * @param string $column Column Name
+     */
     public function __construct(string $column)
     {
         $this->definition = ['column' => $column];
     }
 
-    public function references(string $column): self
+    /**
+     * @param string $column Referance Columen Name
+     * @return self
+     */
+    public function reference(string $column): self
     {
         $this->definition['referenceColumn'] = $column;
         return $this;
     }
 
+    /**
+     * @param string $table Referance Table Name
+     * @return self
+     */
     public function on(string $table): self
     {
         $this->definition['referenceTable'] = $table;
         return $this;
     }
 
+    /**
+     * @param string $action Action On Delete
+     * @return self
+     */
     public function onDelete(string $action): self
     {
         $this->definition['onDelete'] = strtoupper($action);
         return $this;
     }
 
+    /**
+     * @param string $action Action On Update
+     * @return self
+     */
     public function onUpdate(string $action): self
     {
         $this->definition['onUpdate'] = strtoupper($action);
         return $this;
     }
 
+    /**
+     * @param string $name Foreign Key Index Name
+     * @return self
+     */
     public function name(string $name): self
     {
         $this->definition['name'] = $name;
         return $this;
     }
 
+    /**
+     * Get Definitions
+     * @return array
+     */
     public function &getDefinition(): array
     {
         return $this->definition;
